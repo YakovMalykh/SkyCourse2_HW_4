7dc2f20896d5743733e2a27ad2ad97869d54403d
yakov <to-yakoff@mail.ru>
2022-03-25 10:37:53 +0300

    создал проект

---
+++ src/main/java/pro/sky/java/course2/skycourse2_hw_4/SkyCourse2Hw4Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.skycourse2_hw_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyCourse2Hw4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyCourse2Hw4Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/skycourse2_hw_4/SkyCourse2Hw4ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.skycourse2_hw_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyCourse2Hw4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
